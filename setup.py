a6c848d2e0c0449f00f8016737ad1d0d760e7a19
Oliver Bestwalter <oliver.bestwalter@avira.com>
2019-06-26 21:37:39 +0200

    Fix typo in tracker link

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="tox-direct",
     description="plugin for tox - run everything directly (tox creates no virtual env)",
-    version="0.3.4",
+    version="0.3.5",
     packages=find_packages("src"),
     package_dir={"": "src"},
     entry_points={"tox": ["direct = tox_direct.hookimpls"]},
